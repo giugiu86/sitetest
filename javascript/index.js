cb8f8573a1ca202c0cf3ef35bdf0746b36cc3c26
giugiu86 <114621635+giugiu86@users.noreply.github.com>
2023-01-06 22:10:32 +0100

    Create index.js

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+/*Change the properties of a html element*/
+/*document.querySelector(".confirm_button").style.backgroundColor="pink";*/
+
+/*Add other class to existing class*/
+document.querySelector(".confirm_button").classList.add("invisible");
